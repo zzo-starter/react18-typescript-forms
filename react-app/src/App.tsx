b8b90aca59e939a4aa88609dd232d1912fa30de3
Ross Leon <132277790+ross-leon@users.noreply.github.com>
2023-05-06 08:30:40 -0700

    simple form component

---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import Form from "./components/Form";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
